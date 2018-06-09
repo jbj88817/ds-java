59fa9c17096ba3faad01ead6f20402b61a015711
Bojie Jiang <jiangbojie@gmail.com>
2018-06-08 23:49:31 -0700

    Setup LoopQueue

---
+++ src/l3/LoopQueue.java
@@ -0,0 +1,29 @@
+package l3;
+
+public class LoopQueue<E> implements Queue<E> {
+
+    @Override
+    public void enqueue(E e) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E getFront() {
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
