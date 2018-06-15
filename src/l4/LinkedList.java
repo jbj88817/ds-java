c5bf1c67ee93cabcf81c017c3305b1209bf2de6b
Bojie Jiang <jiangbojie@gmail.com>
2018-06-14 23:17:43 -0700

    Node class

--- src/l4/LinkedList.java
+++ src/l4/LinkedList.java
@@ -1,4 +1,43 @@
 package l4;
 
-public class LinkedList {
+public class LinkedList<E> {
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
 }
