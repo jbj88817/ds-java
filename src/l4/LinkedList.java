63646c0aa29772351a4a6acc289781b06021fd1b
Bojie Jiang <jiangbojie@gmail.com>
2018-06-16 23:37:02 -0700

    dummyHead

--- src/l4/LinkedList.java
+++ src/l4/LinkedList.java
@@ -25,11 +25,11 @@ public class LinkedList<E> {
         }
     }
 
-    private Node head;
+    private Node dummyHead;
     private int size;
 
     public LinkedList() {
-        head = null;
+        dummyHead = new Node(null, null);
         size = 0;
     }
 
@@ -41,29 +41,22 @@ public class LinkedList<E> {
         return size == 0;
     }
 
-    public void addFirst(E e) {
-//        Node node = new Node(e);
-//        node.next = head;
-//        head = node;
-
-        head = new Node(e, head);
-        size++;
-    }
-
     public void add(int index, E e) {
         if (index < 0 || index > size) {
             throw new IllegalArgumentException("Illegal index");
         }
-        if (index == 0) {
-            addFirst(e);
-        } else {
-            Node prev = head;
-            for (int i = 0; i < index - 1; i++) {
-                prev = prev.next;
-            }
-            prev.next = new Node(e, prev.next);
-            size++;
+
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
         }
+        prev.next = new Node(e, prev.next);
+        size++;
+
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
     }
 
     public void addLast(E e) {
