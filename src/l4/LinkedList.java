5b7748d24762da08fbd9e2375693de42a32647b4
Bojie Jiang <jiangbojie@gmail.com>
2018-06-17 23:05:56 -0700

    Get Element

--- src/l4/LinkedList.java
+++ src/l4/LinkedList.java
@@ -42,7 +42,7 @@ public class LinkedList<E> {
     }
 
     public void add(int index, E e) {
-        if (index < 0 || index > size) {
+        if (index < 0 || index >= size) {
             throw new IllegalArgumentException("Illegal index");
         }
 
@@ -61,5 +61,66 @@ public class LinkedList<E> {
 
     public void addLast(E e) {
         add(size, e);
+    }
+
+    public E get(int index) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Illegal index");
+        }
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+
+        return cur.e;
+    }
+
+    public E getFirst() {
+        return get(0);
+    }
+
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    public void set(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Illegal index");
+        }
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+//        Node cur = dummyHead.next;
+//        while (cur != null) {
+//            sb.append(cur + "->");
+//            cur = cur.next;
+//        }
+
+        for (Node cur = dummyHead.next; cur != null; cur = cur.next) {
+            sb.append(cur + "->");
+        }
+        sb.append("NULL");
+
+        return sb.toString();
     }
 }
