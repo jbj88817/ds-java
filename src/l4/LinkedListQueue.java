3ee1bc73c6661a0578f2bef022ab872dc0163e92
Bojie Jiang <jiangbojie@gmail.com>
2018-06-20 23:51:00 -0700

    setup LinkedListQueue

---
+++ src/l4/LinkedListQueue.java
@@ -0,0 +1,31 @@
+package l4;
+
+import l3.Queue;
+
+public class LinkedListQueue<E> implements Queue<E> {
+    
+    @Override
+    public void enqueue(E e) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E getFront() {
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
