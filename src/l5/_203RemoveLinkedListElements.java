3270eae7c5649c4808863894f9930b71bd78a357
Bojie Jiang <jiangbojie@gmail.com>
2018-06-22 23:56:52 -0700

    print

--- src/l5/_203RemoveLinkedListElements.java
+++ src/l5/_203RemoveLinkedListElements.java
@@ -18,4 +18,13 @@ public class _203RemoveLinkedListElements {
 
         return dummyHead.next;
     }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 6, 3, 4, 5, 6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        ListNode res = (new _203RemoveLinkedListElements()).removeElements(head, 6);
+        System.out.println(res);
+    }
 }
