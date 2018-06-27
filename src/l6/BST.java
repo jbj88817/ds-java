885b04a2072c823a97251d8e28b5c835b8d6f453
Bojie Jiang <jiangbojie@gmail.com>
2018-06-26 23:09:48 -0700

    contains and preOrder

--- src/l6/BST.java
+++ src/l6/BST.java
@@ -47,4 +47,37 @@ public class BST<E extends Comparable<E>> {
 
         return node;
     }
+
+    public boolean contains(E e) {
+        return contains(root, e);
+    }
+
+    private boolean contains(Node node, E e) {
+        if (node == null) {
+            return false;
+        }
+
+        if (e.compareTo(node.e) == 0) {
+            return true;
+        } else if (e.compareTo(node.e) < 0) {
+            return contains(node.left, e);
+        } else {
+            return contains(node.right, e);
+        }
+    }
+
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+//        if (node == null) {
+//            return;
+//        }
+        if (node != null) {
+            System.out.println(node.e);
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
 }
